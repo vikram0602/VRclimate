12a4ca221d649bbe5c3c8e8dd8207c14cf23a6fd
jgairaud <julian.gairaud1010@gmail.com>
2016-04-25 16:26:54 -0400

    scentist completed

---
+++ Assets/playerVoiceController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerVoiceController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.position = transform.parent.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
